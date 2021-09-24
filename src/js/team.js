3ac970bd553b7b78f51ec3f0916a7e35d65bd2f4
Rom0105 <savosko.photo@gmail.com>
2021-09-24 14:25:57 +0300

    правки

--- src/js/team.js
+++ src/js/team.js
@@ -1,19 +1,3 @@
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelector('[data-modal-open]'),
-//       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       modal: document.querySelector('[data-modal]'),
-//     };
-  
-//     refs.openModalBtn.addEventListener('click', toggleModal);
-//     refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-//     function toggleModal() {
-//       document.body.classList.toggle("modal-open");
-//       refs.modal.classList.toggle('is-hidden');
-//     }
-// })();
-
 const refs = {
   container: document.querySelector('.container-list'),
   modalContainer: document.querySelector('.modal_team-container'),
@@ -22,10 +6,8 @@ const refs = {
 };
 
 refs.heartButton.addEventListener('click', heartButtonClick);
-function heartButtonClick() { 
-  
+function heartButtonClick() {
   refs.modalContainer.classList.add('is-open');
- 
 }
 refs.closModal.addEventListener('click', closeModalClick);
 
@@ -51,7 +33,6 @@ function closelightboxClick(event) {
   closeModalClick();
 }
 
-
 refs.container.addEventListener('click', closelightboxClick);
 
 function closeModalClick() {
@@ -59,7 +40,6 @@ function closeModalClick() {
 }
 
 function closelightboxClick(event) {
- 
   if (event.target.tagName !== 'DIV') {
     return;
   }
